8318ff7ac40f113d3dbfcb4b614d2ae43a36c6cb
Akashs06 <akashskrish@gmail.com>
2023-02-09 02:39:10 +0530

    second

--- src/test/java/Adactin/com/TestRunner/RunnerClass.java
+++ src/test/java/Adactin/com/TestRunner/RunnerClass.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = ".\\src\\test\\java\\feature" , 
 glue = {"Adactin.com.StepDefination" ,"Hooks"},
-tags = "@TC101 and @TC102" , dryRun = false,monochrome = true,stepNotifications = true, 
+tags = "@TC101" , dryRun = false,monochrome = true,stepNotifications = true, 
 plugin = {"html:target\\Report\\Adactin.html","json:target\\Report\\Adactin_Json.json"})
 public class RunnerClass {
 
